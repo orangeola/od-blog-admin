d3cb0d9797287facf9233bf6ef9a2ff1f5c6f0d8
orangeola <orangeqmm@gmail.com>
2023-03-17 01:10:42 +1030

    Edit existing post

--- src/components/Post.js
+++ src/components/Post.js
@@ -5,6 +5,10 @@ import Comment from "./Comment"
 function Post() {
   const {id} = useParams();
   let [post, setPost] = useState(null);
+  let [update, setUpdate] = useState(false);
+  let [title, setTitle] = useState(false);
+  let [text, setText] = useState(false);
+  let [submitted, setSubmitted] = useState(false);
 
   const handleDelP = async () => {
     try{
@@ -28,24 +32,85 @@ function Post() {
     }
   };
 
+  const handleUpdate = async (e) => {
+    e.preventDefault();
+    try{
+      let res = await fetch(`http://localhost:5000/post/${id}/update`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          title: title,
+          text: text,
+       }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('token')),
+        },
+      })
+      let resJson = await res.json();
+      if (res.status === 200) {
+        console.log("Post update", resJson);
+        setPost(resJson.update);
+        setSubmitted(true);
+        prepareForm();
+      } else {
+        console.log("Some error occured", resJson);
+      }
+    }
+    catch(err){
+      console.log("fetch err: ", err);
+    }
+  };
+
   useEffect(() => {
     fetch(`http://localhost:5000/post/${id}`)
    .then(response => response.json())
    .then(data => {
-    console.log(data.post[0])
     setPost(data.post[0]);
+    setTitle(data.post[0].title);
+    setText(data.post[0].text);
     })
-  }, []);
+  }, [submitted]);
+
+  async function prepareForm(){
+    await setUpdate(!update);
+    if(!update){
+      document.getElementsByName('title')[0].value = title;
+      document.getElementsByName('text')[0].value = text;
+    }
+    setSubmitted(false);
+  }
 
   return (
     <div className="Post">
       {post && 
       <div>
         <button onClick={() => {handleDelP()}}>Delete</button>
-        <button>Update</button>
-        <h1>{post.title}</h1>
-        <p>{post.text}</p>
-        <i><p>Posted at: {post.date}</p></i>
+        <button onClick={() => {prepareForm()}}>Update</button>
+        {update &&       
+        <form onSubmit={handleUpdate}>
+          <br></br>
+          <label>
+            Title:
+            <br></br>
+            <input type="text" name="title" onChange={(e) => setTitle(e.target.value)}/>
+          </label>
+          <label>
+            <br></br>
+            Text:
+            <br></br>
+            <textarea type="text" name="text" onChange={(e) => setText(e.target.value)}/>
+          </label>
+          <br></br>
+          <input type="submit" value="Submit" />
+        </form>}
+        <br></br>
+        {!update && 
+          <div>
+            <h1>{post.title}</h1>
+            <p>{post.text}</p>
+            <i><p>Posted at: {post.date}</p></i>   
+          </div> 
+        }
         <Comment />
       </div>
       }
