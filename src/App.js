99b6e77ab1c8b7a27ac02baf4cd088c03fa6dfd1
orangeola <orangeqmm@gmail.com>
2023-03-16 20:05:41 +1030

    Add login page and store token in localStorage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "./components/Home"
 import Post from "./components/Post"
+import Login from "./components/Login"
 
 function App() {
   return (
     <div className="App">
       <div className='Hero'>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/home" element={<Home />} />
           <Route path="/post/:id" element={<Post />} />
           <Route path="*" element={<Home />} />
         </Routes>
---
+++ src/components/Login.js
@@ -0,0 +1,68 @@
+import React, {useState, useEffect} from 'react';
+
+function Login() {  
+  const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try{
+    let res = await fetch(`http://localhost:5000/login`, {
+       method: 'POST',
+       body: JSON.stringify({
+          username: username,
+          password: password,
+       }),
+       headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+       },
+    })
+    let resJson = await res.json();
+    if (res.status === 200) {
+      console.log("Logged in", resJson);
+      setError([{msg: "Logged in", param: "Success"}]);
+      setSuccess(true);
+      localStorage.setItem('token', JSON.stringify(resJson.token));
+      console.log(JSON.parse(localStorage.getItem('token')));
+    } else {
+      console.log("Some error occured", resJson.errors);
+      setError(resJson.errors);
+    }
+  }
+  catch(err){
+    console.log("err: ", err);
+  }
+  };
+
+    return (
+      <div className="Login">
+        <form onSubmit={handleLogin}>
+        <p><b>Login:</b></p>
+        <label>
+          Username:
+          <br></br>
+          <input type="text" name="username" onChange={(e) => setUsername(e.target.value)}/>
+        </label>
+        <label>
+          <br></br>
+          Password:
+          <br></br>
+          <input type="text" name="password" onChange={(e) => setPassword(e.target.value)}/>
+        </label>
+        <br></br><br></br>
+        <input type="submit" value="Submit" />
+        <div>
+          {error && error.map((err) => {
+            return <div key={err.param}>
+              <li>{err.msg}</li>
+            </div>
+          })}
+        </div>
+      </form>
+      </div>
+    );
+  }
+
+export default Login;
